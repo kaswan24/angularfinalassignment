1328ccaf496d2077160ce8bea31dfdd359bd4109
kaswan24 <ankitkaswan04@gmail.com>
2020-04-21 17:44:38 +0530

    created handler in component class

--- src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
+++ src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class Assignment1Component {
 
+  output = '';
+  onInput(inputValue: string): void {  
+    this.output = inputValue;
+  }
+
 }
