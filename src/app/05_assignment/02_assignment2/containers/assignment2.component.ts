0edc4cf9f43b3e12034c55e0163918e1aa64aab1
kaswan24 <ankitkaswan04@gmail.com>
2020-04-22 12:19:53 +0530

    added styles to assignmnet2 rendered table

--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -10,14 +10,18 @@ export class Assignment2Component {
 
   selected:any;
   newComponent:AssignmentComponent = new AssignmentComponent();
-  list =null;
+  list = null;
+  totalRecords = 0;
+  selectedId:any;
 
   constructor(){
     this.list = this.newComponent.todos;
+    this.totalRecords = this.list.length;
   }
 
   select(item) {
     this.selected = (this.selected === item ? null : item); 
+    this.selectedId = (this.selected === item ? this.selected.id : null);
   };
   
   isActive(item) {
